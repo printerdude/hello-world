497bafe9b26ff6fcd5bba5bfac1d30f2a52502a6
Brian Yoon <infobyoon@gmail.com>
2022-11-02 17:57:33 -0700

    add contract instance to update.js

--- scripts/update.js
+++ scripts/update.js
@@ -2,9 +2,18 @@ const API_KEY = process.env.API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 
-const { ethers } = require("hardhat");
+const { ethers, network } = require("hardhat");
 
 const contract = require('../artifacts/contracts/HelloWorld.sol/HelloWorld.json');
+
+// provider - Alchemy
+const alchemyProvider = new ethers.providers.AlchemyProvider(network="goerli", API_KEY);
+
+// signer - me
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
+
+// contract instance
+const helloWorldContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
 
 async function main() {
     // place new message you want to put in the update here
