4b73c351586e9293fd21c6c323e891e3cdbcd655
Brian Yoon <infobyoon@gmail.com>
2022-11-02 17:36:35 -0700

    add contract ABI to update.js

--- scripts/update.js
+++ scripts/update.js
@@ -1,3 +1,18 @@
+const API_KEY = process.env.API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+
+const { ethers } = require("hardhat");
+
+const contract = require('../artifacts/contracts/HelloWorld.sol/HelloWorld.json');
+
 async function main() {
-    
+    // place new message you want to put in the update here
 }
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
