311eee2166d8b40ed0fef3c64a88a926f6c2cdcd
Brian Yoon <infobyoon@gmail.com>
2022-11-02 18:46:01 -0700

    debugged invalid url and constant variable errors

--- scripts/update.js
+++ scripts/update.js
@@ -2,12 +2,12 @@ const API_KEY = process.env.API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 
-const { ethers, network } = require("hardhat");
+const { ethers } = require("hardhat");
 
 const contract = require('../artifacts/contracts/HelloWorld.sol/HelloWorld.json');
 
 // provider - Alchemy
-const alchemyProvider = new ethers.providers.AlchemyProvider(network="goerli", API_KEY);
+const alchemyProvider = new ethers.providers.AlchemyProvider(network = "goerli", API_KEY);
 
 // signer - me
 const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
@@ -19,14 +19,14 @@ async function main() {
 	// place new message you want to put in the update here
 
 	const message = await helloWorldContract.message();
-	console.log(`This message says ${message}`);
+	console.log(`This message says: ${message}`);
 
     console.log("Updating the message...");
-    tx = await helloWorldContract.update('this is the new message');
+    tx = await helloWorldContract.update('gobble gobble');
     await tx.wait();
 
     const newMessage = await helloWorldContract.message();
-	console.log(`This new message says ${newMessage}`);
+	console.log(`This new message says: ${newMessage}`);
 
 }
 
